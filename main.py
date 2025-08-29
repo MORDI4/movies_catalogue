4b98c808de16d8e71921db35dc96c42ded956598
Michal Mordarski <mmordarski4@gmail.com>
2025-08-29 17:07:09 +0200

    Edit homepage

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+from faker import Faker
+
+f = Faker('pl_PL')
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
     movies = []
+    for i in range(10):
+        movies.append(f.text(max_nb_chars=20).strip('.'))
     return render_template("homepage.html", movies=movies)
 
 if __name__ == '__main__':
